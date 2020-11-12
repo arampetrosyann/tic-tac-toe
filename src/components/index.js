2f6101b72c55b0ce22985f90be62bbd36be76355
arampetrosyan <0aramp@gmail.com>
2020-11-13 02:31:18 +0400

    [ci] export components from one file

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Square } from "./Square/Square";
+export { default as Board } from "./Board/Board";
+export { default as WinBox } from "./WinBox/WinBox";
+export { default as Button } from "./Button/Button";
